ee542c48bd2d837ede87cd35ad40242ad65682b5
Sherwin Price <sherwin.price@hotmmail.com>
2018-04-15 11:20:55 -0400

    add spring context, and views

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # webear
 JAVA EE sample ear module project
+ use spring release - https://jira.spring.io/browse/SPR-16591 for to support weblogic deployments
--- common/src/main/java/controllers/IndexController.java
+++ common/src/main/java/controllers/IndexController.java
@@ -4,19 +4,24 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns= {"/index.html","/"})
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@ControllerAdvice
 public class IndexController extends HttpServlet{
 
 	private static final long serialVersionUID = 8036731501362521420L;
-
-	 public void doGet(HttpServletRequest request, HttpServletResponse response)
+	@RequestMapping(value= {"/index.html","/"})
+	public ModelAndView index(HttpServletRequest request, HttpServletResponse response)
 	            throws IOException, ServletException {
-		 RequestDispatcher view = getServletContext().getRequestDispatcher("/WEB-INF/views/index.jsp");
-		 view.forward(request, response);
+		 
+		 return new ModelAndView("index");
 	 }
 }
